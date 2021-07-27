865bd720ff00972f06a1286cec6d2965d90e4855
Ksenia Baranova <baranova-ksenia@mail.ru>
2021-07-27 21:06:02 +0300

    myside video

---
+++ src/components/Options.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {
+  Button,
+  TextField,
+  Grid,
+  Typography,
+  Container,
+  Paper,
+} from "@material-ui/core";
+
+const Options = ({ children }) => {
+  return (
+    <div>
+      Options
+      {children}
+    </div>
+  );
+};
+
+export default Options;
